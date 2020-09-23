c1fd6609eff6f07eeba4f880e0197a60bcccf817
Dusty Lefevre <dusty.lefevre@flowfactor.be>
2020-09-24 00:00:17 +0200

    Add generator kickstart script

---
+++ generator.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+case $1 in
+  start)
+    ansible -f 25 -i inventory workload -m shell -a "cd soap-traffic-generator && nohup ./generator.py &"
+    ;;
+  stop)
+    ansible -f 25 -i inventory workload -m shell -a "cd soap-traffic-generator && ./generator.py --threads=-1"
+    ;;
+  threads)
+    ansible -f 25 -i inventory workload -m shell -a "cd soap-traffic-generator && ./generator.py --threads=$2"
+    ;;
+  *)
+    echo "Unknown command. Use: start, stop, threads"
+    ;;
+esac
